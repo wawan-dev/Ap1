f953d0c6b2705ed4a496a1831c6438db86a291cb
Erwan Girard <erwangirard49500@gmail.com>
2024-09-13 17:02:19 +0200

    fin seance

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -13,12 +13,16 @@ class MainController extends Controller
     public function home()
     {
         // Récuération du hackathon actif (celui en cours)
+        
         $hackathon = Hackathon::getActiveHackathon();
+
+        $nbplace = Hackathon::count();
 
         // Affichage de la vue, avec les données récupérées
         return view('main.home', [
             'hackathon' => $hackathon,
             'organisateur' => $hackathon->organisateur,
+            'nbplace' => $nbplace,
         ]);
     }
 
--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -36,8 +36,12 @@
                 @endif
 
                 <div class="d-flex flex-wrap pt-3">
-                    <a class="btn bg-green m-2 button-home" href="/join?idh=<?= $hackathon->idhackathon ?>">Rejoindre</a>
-                    <a class="btn bg-green m-2 button-home" href="{{route("create-team")}}">Créer mon équipe</a>
+                    $place = ge
+                    @if($hackathon->nbplaceeqmax != $nbplace ){
+                        <a class="btn bg-green m-2 button-home" href="/join?idh=<?= $hackathon->idhackathon ?>">Rejoindre</a>
+                        <a class="btn bg-green m-2 button-home" href="{{route('create-team')}}">Créer mon équipe</a>
+                    }
+
                     <a class="btn bg-green m-2 button-home" href="#" @click.prevent="getParticipants">
                         <span v-if="!loading">Les participants</span>
                         <span v-else>Chargement en cours…</span>
