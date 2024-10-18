0cd8c4df7c2a9b100519c544032692e5a9f1632f
Girard Erwan <erwangirard49500@gmail.com>
2024-10-18 16:35:13 +0200

    filtre avec en cours, passer et futur

--- app/Http/Controllers/historiqueController.php
+++ app/Http/Controllers/historiqueController.php
@@ -16,9 +16,21 @@ class historiqueController extends Controller
     public function filtrer_n(Request $request)
     {
         $query = Hackathon::query();
+        $currentDate = now();
 
         if ($search = $request->get('search')) {
             $query->where('thematique', 'like', '%' . $search . '%');
+        }
+
+        if ($status = $request->get('status')) {
+            if ($status == 'a_venir') {
+                $query->where('dateheuredebuth', '>', $currentDate);
+            } elseif ($status == 'en_cours') {
+                $query->where('dateheuredebuth', '<=', $currentDate)
+                      ->where('dateheurefinh', '>=', $currentDate);
+            } elseif ($status == 'passe') {
+                $query->where('dateheurefinh', '<', $currentDate);
+            }
         }
 
         $hackathon = $query->paginate(5); // Pagination
--- resources/views/historique.blade.php
+++ resources/views/historique.blade.php
@@ -13,17 +13,29 @@
 
             <!-- Formulaire de recherche -->
             <form method="GET" action="{{ route('filtrer_n') }}" class="d-flex justify-content-center align-items-center mb-3">
+                 @csrf
                 <label for="search" class="me-2">Rechercher par nom :</label>
                 <input type="text" name="search" id="search" class="form-control w-50" placeholder="Nom de l'hackathon" value="{{ request()->get('search') }}">
+                
+
+                <select name="status" id="status" class="form-select m-5">
+                    <option value="">Tous</option>
+                    <option value="a_venir" {{ request()->get('status') == 'a_venir' ? 'selected' : '' }}>À venir</option>
+                    <option value="en_cours" {{ request()->get('status') == 'en_cours' ? 'selected' : '' }}>En cours</option>
+                    <option value="passe" {{ request()->get('status') == 'passe' ? 'selected' : '' }}>Passé</option>
+                </select>
+
                 <button type="submit" class="btn btn-primary ms-2">Rechercher</button>
             </form>
 
             @foreach ($hackathon as $h)
                 <!-- Chaque hackathon est espacé avec mb-3 et les boutons sont alignés -->
-                <div class="d-flex justify-content-between align-items-center mb-2"> 
-                     <p class="mb-0">{{ $h->thematique }}</p>
+                <div class="d-flex align-items-center mb-2"> 
+                     <p class="mb-0"><h3>{{ $h->thematique }}</h3> &nbsp; &nbsp; &nbsp; {{$h->dateheuredebuth}} / {{$h->dateheurefinh}}</p>
+                     
                     <a href="/" class="btn bg-green m-2 button-home">Détail de l'hackathon</a>
                 </div>
+                
             @endforeach
         </div>
 
