19abf21d81e1fb0d90ee550f86f24cfa25117ebe
Girard Erwan <erwangirard49500@gmail.com>
2024-11-14 23:04:48 +0100

    Changement affichage des membre de l'équipe

--- resources/views/membre/affiche.blade.php
+++ resources/views/membre/affiche.blade.php
@@ -4,20 +4,29 @@
 
 @section('content')
 
-
-<div class="d-flex flex-column justify-content-center min-vh-100 align-items-center">
-<br>
-    <h1>Liste des Membre :</h1>
-    <br>
-    @foreach($lesmembres as $membre)
-        
-        <p> {{$membre->nom}}  {{$membre->prenom}} </p>
-    @endforeach
-    <br>
-    <br>
-    <a class="btn bg-info m-2 button-home" href="/"  @click.prevent="participantsIsShown = true"><--</a>
-</div>
-
-
-
+@section('custom-css')
+    <link href="/css/login.css" rel="stylesheet"/>
 @endsection
+
+@section('content')
+    <div class="d-flex flex-column justify-content-center align-items-center min-vh-100 bg fullContainer" >
+        <div class="wrapper fadeInDown" >
+            <div class="cardRadius">
+                <br>
+                <h1>Liste des membres :</h1>
+                <br>
+                @foreach($lesmembres as $membre)
+                    
+                    <p> {{$membre->nom}}  {{$membre->prenom}} </p>
+                @endforeach
+                <br>
+                <br>
+                <a class="btn btn-primary mt-3 fadeIn third m-5" href="/"  @click.prevent="participantsIsShown = true"><--</a>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+
+
